68787725e81347ebb509e8d2047fed9699f310dc
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-03-08 16:32:53 +0530

    fetched api

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 import styles from "../styles/Home.module.css";
+import requests from "../utils/requests";
 export default function Home() {
   return (
     <div>
@@ -13,6 +15,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Results />
     </div>
   );
 }
+
+// export async function getServerSideProps(context) {
+//   const genre = context.query.genre;
+
+//   const request = await fetch(
+//     `https://api.themoviedb.org/3${
+//       requests[genre]?.url || requests.fetchTrending.url
+//     }`
+//   ).then((res) => res.json());
+
+//   return {
+//     props: {
+//       results: request.results,
+//     },
+//   };
+// }
