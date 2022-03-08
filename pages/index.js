6bccce3c77cb3257d47dba8cb6e650b59e157b74
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-03-08 14:28:54 +0530

    Navbar completed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Nav />
     </div>
   );
 }
