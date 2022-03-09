be9fb404f4f6230c88249aabbf8c5f8bf1ccbac4
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-03-09 11:59:42 +0530

    result completed

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,13 @@
-function Results() {
-  return <div>Results</div>;
+import Thumbnail from "./Thumbnail";
+
+function Results({ results }) {
+  return (
+    <div className="px-5 py-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import Nav from "../components/Nav";
 import Results from "../components/Results";
 import styles from "../styles/Home.module.css";
 import requests from "../utils/requests";
-export default function Home() {
+export default function Home({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -15,23 +16,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const genre = context.query.genre;
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
 
-//   const request = await fetch(
-//     `https://api.themoviedb.org/3${
-//       requests[genre]?.url || requests.fetchTrending.url
-//     }`
-//   ).then((res) => res.json());
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
 
-//   return {
-//     props: {
-//       results: request.results,
-//     },
-//   };
-// }
+  return {
+    props: {
+      results: request.results,
+    },
+  };
+}
